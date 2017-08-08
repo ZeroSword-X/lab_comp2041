a18fef9b8f8dadf84301860ebf4ed33ecc9c312e
Kelvin FAN <comp192716s2@gmail.com>
2017-08-09 00:34:50 +1000

    Week03 Lab Challenging Exercise

--- week03/courses.sh
+++ week03/courses.sh
@@ -1,10 +1,47 @@
 #!/bin/sh
 
-#IFS="$"
+first_Letter=`echo "$1" | cut -c1`
 
-list=`wget -q -O- "http://www.handbook.unsw.edu.au/vbook2017/brCoursesByAtoZ.jsp?StudyLevel=Undergraduate&descr=O" | grep OPTM | sed -e 's/<[^<>]*>//g' | sed -e 's/\t//g'`
+#Get the course list from the undergraduate handbook first
+courses=`wget -q -O- "http://www.handbook.unsw.edu.au/vbook2017/brCoursesByAtoZ.jsp?StudyLevel=Undergraduate&descr=$first_Letter" | egrep $1 | egrep '\<TD' | sed -e 's/<[^<>]*>//g' -e 's/\t//g'` 
 
-for line in "$list"
+output=""
+
+for i in $courses
 do
-	echo "1"
+	echo "$i" | egrep "$1[0-9]{4}" &> /dev/null
+
+	if [ $? -eq 0 ] 
+	then
+		if [ "$output" == "" ]
+		then
+    		output="$i"
+		else
+			output="$output"$'\n'"$i"
+		fi
+	else
+		output="$output $i"
+	fi
 done
+
+#Get the course list from postgraduate handbook
+courses=`wget -q -O- "http://www.handbook.unsw.edu.au/vbook2017/brCoursesByAtoZ.jsp?StudyLevel=Postgraduate&descr=$first_Letter" | egrep $1 | egrep "\<TD" | sed -e 's/<[^<>]*>//g' -e 's/\t//g'` 
+
+for i in $courses
+do
+	echo "$i" | egrep "$1[0-9]{4}" &> /dev/null
+
+	if [ $? -eq 0 ] 
+	then
+		if [ "$output" == "" ]
+		then
+    		output="$i"
+		else
+			output="$output"$'\n'"$i"
+		fi
+	else
+		output="$output $i"
+	fi
+done
+
+echo "$output" | sort | uniq
