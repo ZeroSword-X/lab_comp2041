9da014f62fc3a53cf3b2904458e81dff2e1141bc
Kelvin FAN <comp192716s2@gmail.com>
2017-08-13 23:32:11 +1000

    Week04 Lab

---
+++ week04/email_image.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]
+then
+   printf "Usage: ./email_image.sh <image file(s)>\n"
+   exit 1
+fi
+
+
+exit_status=0
+
+for image in "$@"
+do
+   if ! display $image &> /dev/null 
+   then
+      printf "Address to e-mail this image to? "
+      read email
+
+      if printf "%s" "$email" | egrep "^[a-zA-Z0-9_]+\@[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+$" > /dev/null
+      then
+         printf "Message to accompany image? "
+         read msg
+         
+         if printf "%s" "$msg" | mutt -s "UNSW COMP2041 Testing" -a "$image" -e set -- "$email" &> /dev/null
+         then
+            printf "$image sent to $email\n"
+         else
+            printf "Error: Unable to sent \"$image\"\n"
+            exit_status=1
+         fi
+      else
+         printf "$email is not a valid email address. $image will not be sent\n"
+         exit_status=1
+         continue
+      fi
+
+   else
+      printf "\"$image\" cannot be displayed. The image will not be sent\n"
+      exit_status=1
+      continue
+   fi
+done
+
+exit $exit_status
---
+++ week04/jpg2png.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# If there is no jpg file, then just exit the shell script (with exit status 1)
+if ! ls *.jpg &> /dev/null 
+then
+   printf "Error: There is no .jpg file is in the current directory !!\n"
+   exit 1
+fi
+
+
+exit_status=0
+
+for image in *.jpg
+do
+   new_image=`printf "%s" "$image" | sed -e "s/.jpg/.png/"`
+
+   if test -e "$new_image"
+   then
+      printf "$new_image already exists\n"
+      exit_status=1
+      continue
+   fi
+
+   convert "$image" "$new_image" && rm -- "$image"
+done
+
+exit $exit_status
