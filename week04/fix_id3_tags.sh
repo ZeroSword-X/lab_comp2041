672972ea4e9f053bf610548e831d08dbc1ed89ca
Kelvin FAN <comp192716s2@gmail.com>
2017-08-14 19:39:27 +1000

    Add fix_id3_tags.sh

--- week04/fix_id3_tags.sh
+++ week04/fix_id3_tags.sh
@@ -6,24 +6,43 @@ then
    exit 1
 fi
 
+
+exit_status=0
+TMP_FILE=/tmp/fix_id3_tags_tmp$$
+
 for arg in "$@"
 do
    if test -d "$arg"
    then
-      continue     
+      find "$arg" -name '*.mp3' -print > $TMP_FILE
+
+      while read line
+      do
+         album=`printf "%s" "$line" | cut -d'/' -f2`
+         year=`printf "%s" "$album" | cut -d',' -f2 | cut -c2-`
+
+         track=`printf "%s" "$line" | cut -d'/' -f3 | sed -e 's/^\(.\+\) - \(.\+\) - \(.\+\)$/\1/g'` 
+         title=`printf "%s" "$line" | cut -d'/' -f3 | sed -e 's/^\(.\+\) - \(.\+\) - \(.\+\)$/\2/g'` 
+         artist=`printf "%s" "$line" | cut -d'/' -f3 | sed -e 's/^\(.\+\) - \(.\+\) - \(.\+\)$/\3/g' -e 's/.mp3//g'` 
+
+         id3 -a "$artist" -A "$album" -t "$title" -T "$track" -y "$year" "$line" > /dev/null
+      done < $TMP_FILE     
+
+      rm -f $TMP_FILE
    elif test -e "$arg"
    then
-      path=`printf "%s" "$arg" | sed -e 's/ - /-/g'`
-      album=`printf "%s" "$path" | cut -d'/' -f2`
+      album=`printf "%s" "$arg" | cut -d'/' -f2`
       year=`printf "%s" "$album" | cut -d',' -f2 | cut -c2-`
 
-      track=`printf "%s" "$path" | cut -d'/' -f3 | cut -d'-' -f1` 
-      title=`printf "%s" "$path" | cut -d'/' -f3 | cut -d'-' -f2` 
-      artist=`printf "%s" "$path" | cut -d'/' -f3 | cut -d'-' -f3 | sed -e 's/.mp3//g'` 
+      track=`printf "%s" "$arg" | cut -d'/' -f3 | sed -e 's/^\(.\+\) - \(.\+\) - \(.\+\)$/\1/g'` 
+      title=`printf "%s" "$arg" | cut -d'/' -f3 | sed -e 's/^\(.\+\) - \(.\+\) - \(.\+\)$/\2/g'` 
+      artist=`printf "%s" "$arg" | cut -d'/' -f3 | sed -e 's/^\(.\+\) - \(.\+\) - \(.\+\)$/\3/g' -e 's/.mp3//g'` 
 
-
-      #printf "%s\n" "$album"
-      #printf "%s\n" "$artist"
-      #printf "%s\n" "$title"
+      id3 -a "$artist" -A "$album" -t "$title" -T "$track" -y "$year" "$arg" > /dev/null
+   else
+      printf "\'$arg\' does not exist !\n"
+      exit_status=1
    fi
 done
+
+exit $exit_status
