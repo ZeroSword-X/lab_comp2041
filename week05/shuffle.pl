6fe380d31b40b12549b23c1e0a8a237047835d1d
ZeroSwordEX <kelvinfan0720@gmail.com>
2017-08-21 02:18:08 +1000

    Add shuffle.pl

--- week05/shuffle.pl
+++ week05/shuffle.pl
@@ -1,3 +1,25 @@
 #!/usr/bin/perl -w
 
+@array = ();
+while ($line = <STDIN>) {
+   chomp $line;
+   push @array, $line;
+}
 
+$i = 0;
+$n = @array;
+
+for($i = 0; $i < $n; $i++) {
+   $p = int(rand($n));
+   $q = int(rand($n));
+
+   if ($p != $q) {
+      $tmp = $array[$p];
+      $array[$p] = $array[$q];
+      $array[$q] = $tmp;
+   }
+}
+
+foreach $i (@array) {
+   print "$i\n";
+}
